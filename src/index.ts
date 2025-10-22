ed5a6db3cf9d68f4f0ed6ebe0d58c1e6cf9314dc
swimly <979741120@qq.com>
2025-10-22 21:12:28 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @ldesign/upload - 上传组件
+ */
+export class Uploader {
+  async upload(file: File) { console.info('Uploading:', file.name); return { success: true } }
+}
+export function createUploader() { return new Uploader() }
+
